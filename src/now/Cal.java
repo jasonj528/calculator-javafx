5673d572f747af477d0199247f27becbf813c2bc
rn <nieblingr1@nku.edu>
2017-03-10 09:12:46 -0500

    code cleanup

--- src/now/Cal.java
+++ src/now/Cal.java
@@ -29,7 +29,6 @@ public class Cal extends Application {
 	public static final double YSCL = .01;
 
 	boolean x2 = false;
-
 	boolean res = false;
 
 	String exp = "";
@@ -43,59 +42,63 @@ public class Cal extends Application {
 		mainPane.setStyle("-fx-background-color: #000000;");
 
 		HBox hb = new HBox(10);
-		hb.getChildren().addAll(); // Can add buttons here. This is the Hbox
-									// below the text box and above the numpad
-									// buttons. hb.getChildren().addAll(your,
-									// buttons, go, here);
+		hb.getChildren().addAll(); // Can add buttons here. This is the Hbox below the text box and above the numpad
+									                 // buttons. hb.getChildren().addAll(your, buttons, go, here);
 
 		TextField textField = new TextField();
-
 		textField.prefHeightProperty().bind(mainPane.heightProperty().divide(3));
 		textField.setStyle("-fx-font-size:29px;");
-
 		textField.setAlignment(Pos.BOTTOM_RIGHT);
+		
 		Button num9 = new Button("9");
 		num9.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num9.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num9.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num9.styleProperty().bind(Bindings.when(num9.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button num8 = new Button("8");
 		num8.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num8.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num8.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num8.styleProperty().bind(Bindings.when(num8.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button num7 = new Button("7");
 		num7.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num7.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num7.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num7.styleProperty().bind(Bindings.when(num7.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button div = new Button("/");
 		div.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		div.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		div.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		div.styleProperty().bind(Bindings.when(div.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: #434343;")));
+		
 		Button num6 = new Button("6");
 		num6.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num6.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num6.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num6.styleProperty().bind(Bindings.when(num6.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button num5 = new Button("5");
 		num5.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num5.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num5.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num5.styleProperty().bind(Bindings.when(num5.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button num4 = new Button("4");
 		num4.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num4.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num4.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num4.styleProperty().bind(Bindings.when(num4.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button mul = new Button("*");
 		mul.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		mul.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -103,18 +106,21 @@ public class Cal extends Application {
 		mul.styleProperty()
 				.bind(Bindings.when(mul.hoverProperty()).then(new SimpleStringProperty("-fx-color: #949494;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #2e2e2e;")));
+		
 		Button b1 = new Button("(");
 		b1.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		b1.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		b1.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		b1.styleProperty().bind(Bindings.when(b1.hoverProperty()).then(new SimpleStringProperty("-fx-color: #4f7aa2;"))
 				.otherwise(new SimpleStringProperty("-fx-color: #34516e;")));
+		
 		Button b2 = new Button(")");
 		b2.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		b2.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		b2.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		b2.styleProperty().bind(Bindings.when(b2.hoverProperty()).then(new SimpleStringProperty("-fx-color: #4f7aa2;"))
 				.otherwise(new SimpleStringProperty("-fx-color: #34516e;")));
+		
 		Button backSpace = new Button("\u2190");
 		backSpace.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		backSpace.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -122,6 +128,7 @@ public class Cal extends Application {
 		backSpace.styleProperty()
 				.bind(Bindings.when(backSpace.hoverProperty()).then(new SimpleStringProperty("-fx-color: #4f7aa2;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #34516e;")));
+		
 		Button home = new Button("\u2302");
 		home.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		home.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -129,24 +136,28 @@ public class Cal extends Application {
 		home.styleProperty()
 				.bind(Bindings.when(home.hoverProperty()).then(new SimpleStringProperty("-fx-color: #4f7aa2;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #34516e;")));
+		
 		Button num3 = new Button("3");
 		num3.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num3.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num3.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num3.styleProperty().bind(Bindings.when(num3.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button num2 = new Button("2");
 		num2.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num2.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num2.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num2.styleProperty().bind(Bindings.when(num2.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button num1 = new Button("1");
 		num1.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num1.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
 		num1.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num1.styleProperty().bind(Bindings.when(num1.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button sub = new Button("-");
 		sub.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		sub.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -154,6 +165,7 @@ public class Cal extends Application {
 		sub.styleProperty()
 				.bind(Bindings.when(sub.hoverProperty()).then(new SimpleStringProperty("-fx-color: #949494;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #2e2e2e;")));
+		
 		Button xPower2 = new Button("x²");
 		xPower2.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		xPower2.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -161,6 +173,7 @@ public class Cal extends Application {
 		xPower2.styleProperty()
 				.bind(Bindings.when(xPower2.hoverProperty()).then(new SimpleStringProperty("-fx-color: #4f7aa2;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #34516e;")));
+		
 		Button sqrt = new Button("\u221A");
 		sqrt.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		sqrt.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -168,13 +181,14 @@ public class Cal extends Application {
 		sqrt.styleProperty()
 				.bind(Bindings.when(sqrt.hoverProperty()).then(new SimpleStringProperty("-fx-color: #4f7aa2;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #34516e;")));
+		
 		Button num0 = new Button("0");
 		num0.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		num0.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
-		// num0.setStyle("-fx-line-height: 1.8;");
 		num0.setFont(Font.font("SansSerif", FontWeight.BOLD, 14));
 		num0.styleProperty().bind(Bindings.when(num0.hoverProperty()).then(new SimpleStringProperty("-fx-color: gray;"))
 				.otherwise(new SimpleStringProperty("-fx-color: white;")));
+		
 		Button dot = new Button(".");
 		dot.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		dot.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -182,6 +196,7 @@ public class Cal extends Application {
 		dot.styleProperty()
 				.bind(Bindings.when(dot.hoverProperty()).then(new SimpleStringProperty("-fx-color: #949494;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #2e2e2e;")));
+		
 		Button mod = new Button("%");
 		mod.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		mod.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -189,6 +204,7 @@ public class Cal extends Application {
 		mod.styleProperty()
 				.bind(Bindings.when(mod.hoverProperty()).then(new SimpleStringProperty("-fx-color: #949494;"))
 						.otherwise(new SimpleStringProperty("-fx-color: #2e2e2e;")));
+		
 		Button sum = new Button("+");
 		sum.prefHeightProperty().bind(mainPane.heightProperty().divide(5));
 		sum.prefWidthProperty().bind(mainPane.widthProperty().divide(6));
@@ -217,14 +233,17 @@ public class Cal extends Application {
 		hBox1.prefHeightProperty().bind(primaryStage.heightProperty().divide(4));
 		hBox1.prefWidthProperty().bind(primaryStage.widthProperty());
 		hBox1.getChildren().addAll(num7, num8, num9, div, backSpace, home);
+		
 		HBox hBox2 = new HBox(5);
 		hBox2.prefHeightProperty().bind(primaryStage.heightProperty().divide(4));
 		hBox2.prefWidthProperty().bind(primaryStage.widthProperty());
 		hBox2.getChildren().addAll(num4, num5, num6, mul, b1, b2);
+		
 		HBox hBox3 = new HBox(5);
 		hBox3.prefHeightProperty().bind(primaryStage.heightProperty().divide(4));
 		hBox3.prefWidthProperty().bind(primaryStage.widthProperty());
 		hBox3.getChildren().addAll(num1, num2, num3, sub, xPower2, sqrt);
+		
 		HBox hBox4 = new HBox(5);
 		hBox4.prefHeightProperty().bind(primaryStage.heightProperty().divide(4));
 		hBox4.prefWidthProperty().bind(primaryStage.widthProperty());
@@ -267,15 +286,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "0";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num1.setOnMouseClicked(e -> {
 			String x = "1";
 			exp += x;
@@ -283,15 +300,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "1";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num2.setOnMouseClicked(e -> {
 			String x = "2";
 			exp += x;
@@ -299,15 +314,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "2";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num3.setOnMouseClicked(e -> {
 			String x = "3";
 			exp += x;
@@ -315,15 +328,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "3";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num4.setOnMouseClicked(e -> {
 			String x = "4";
 			exp += x;
@@ -331,15 +342,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "4";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num5.setOnMouseClicked(e -> {
 			String x = "5";
 			exp += x;
@@ -347,15 +356,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "5";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num6.setOnMouseClicked(e -> {
 			String x = "6";
 			exp += x;
@@ -363,15 +370,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "6";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num7.setOnMouseClicked(e -> {
 			String x = "7";
 			exp += x;
@@ -379,15 +384,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "7";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num8.setOnMouseClicked(e -> {
 			String x = "8";
 			exp += x;
@@ -395,15 +398,13 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "8";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
+		
 		num9.setOnMouseClicked(e -> {
 			String x = "9";
 			exp += x;
@@ -411,14 +412,11 @@ public class Cal extends Application {
 			if (res == true) {
 				exp = "";
 				textField.setText(exp);
-
 				String x2 = "9";
 				exp += x2;
 				textField.setText(exp);
 				res = false;
-
 			}
-
 		});
 
 		graph.setOnMouseClicked(e -> {
@@ -435,88 +433,74 @@ public class Cal extends Application {
 			graphingStage.setScene(new Scene(root));
 			// show scene
 			graphingStage.show();
-
 		});
 
 		dot.setOnMouseClicked(e -> {
-
 			if (x2 == false) {
-
 				String x = ".";
 				exp += x;
 				textField.setText(exp);
 				res = false;
 				x2 = true;
 			}
-
 		});
+		
 		mod.setOnMouseClicked(e -> {
 			String x = "%";
 			exp += x;
 			textField.setText(exp);
 			res = false;
 			x2 = false;
-
 		});
+		
 		mul.setOnMouseClicked(e -> {
-
 			String x = "*";
 			exp += x;
 			textField.setText(exp);
 			res = false;
-
 			x2 = false;
-
 		});
+		
 		div.setOnMouseClicked(e -> {
 			String x = "/";
 			exp += x;
 			textField.setText(exp);
 			res = false;
 			x2 = false;
-
 		});
+		
 		sum.setOnMouseClicked(e -> {
 			String x = "+";
 			exp += x;
 			textField.setText(exp);
 			res = false;
 			x2 = false;
-
 		});
 
 		sub.setOnMouseClicked(e -> {
-
 			String x = "-";
 			exp += x;
 			textField.setText(exp);
 			res = false;
-
 			x2 = false;
-
 		});
 
 		equal.setOnMouseClicked(e -> {
 			try {
-				
 				exp = exam.solve2(exp);
 				textField.setText(exp);
 				res = true;
 				x2 = false;
-
 			} catch (NullPointerException eed) {
 			} catch (NumberFormatException eed) {
 			}
 		});
 
 		home.setOnMouseClicked(e -> {
-
 			exp = "";
 			textField.setText(exp);
 			x2 = false;
-
 			res = false;
-
 		});
 		backSpace.setOnMouseClicked(e -> {
 			try {
@@ -525,11 +509,8 @@ public class Cal extends Application {
 				i++;
 				textField.setText(exp);
 				res = false;
-
 				x2 = false;
-
 			} catch (IndexOutOfBoundsException ees) {
-
 			}
 		});
 
@@ -558,28 +539,25 @@ public class Cal extends Application {
 			String u = exp.substring(yy2, yy);
 			exp = "";
 			exp = h + "(" + u + "^" + ")";
-
 			textField.setText(exp);
 			res = false;
-
 			x2 = false;
 		});
+		
 		b1.setOnMouseClicked(e -> {
-
 			exp += "(";
 			textField.setText(exp);
 			res = false;
 			x2 = false;
-
 		});
+		
 		b2.setOnMouseClicked(e -> {
-
 			exp += ")";
 			textField.setText(exp);
 			res = false;
 			x2 = false;
-
 		});
+		
 		sqrt.setOnMouseClicked(e -> {
 			exp += "S" + "(";
 			textField.setText(exp);
@@ -593,20 +571,6 @@ public class Cal extends Application {
 		mainPane.getChildren().addAll(settingsBox, textField, hb, hBox1, hBox2, hBox3, hBox4);
 		Scene scene = new Scene(mainPane, 410, 500);
 		primaryStage.setTitle("Calculator");
-		/*
-		 * 
-		 * android project
-		 * 
-		 * 
-		 * primaryStage.setScene(scene); primaryStage.show(); Rectangle2D
-		 * visualBounds = Screen.getPrimary().getVisualBounds(); Scene scene =
-		 * new Scene(mainPane, visualBounds.getWidth(),
-		 * visualBounds.getHeight());
-		 * 
-		 * primaryStage.getIcons().add(new
-		 * Image(Project.class.getResourceAsStream("/icon.png")));
-		 */
-
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 
